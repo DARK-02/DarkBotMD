cc0ff2aaedaebb5e5e44be4e64fd1525c7a74a87
DARK-02 <76576010+DARK-02@users.noreply.github.com>
2022-04-16 13:28:23 +0700

    Create dark-simi.py

---
+++ dark-simi.py
@@ -0,0 +1,11 @@
+import os,sys,requests,shlex
+from requests import get
+mr_dark = " ".join(map(shlex.quote, sys.argv[1:]))
+mr_dark_url = f'http://api.simsimi.net/v2/?text={mr_dark}&lc=id'
+dark = requests.get(mr_dark_url)
+if dark.status_code == 200:
+     print('')
+     print('_simi bot_: ', dark.json().get('success'))
+     print('```Copyright By Mr_Dark 2022```')
+else:
+     print('bad respon ')
