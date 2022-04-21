e2afbf1e0ddafee2b12923f17c2c0a8f44adeaef
DARK-02 <76576010+DARK-02@users.noreply.github.com>
2022-04-21 17:20:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function startHisoka() {
     const hisoka = hisokaConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Hisoka Multi Device','Safari','1.0.0'],
+        browser: ['DarkBot','Safari','1.0.0'],
         auth: state
     })
 
