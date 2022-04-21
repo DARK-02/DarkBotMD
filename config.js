d9bc653933d86288153886ab921b4aac40bbd34a
DARK-02 <76576010+DARK-02@users.noreply.github.com>
2022-04-21 17:26:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ const chalk = require('chalk')
 
 // Website Api
 global.APIs = {
-	zenz: 'https://zenzapi.xyz',
+	zenz: 'https://zenzapis.xyz',
 }
 
 // Apikey Website Api
 global.APIKeys = {
-	'https://zenzapi.xyz': 'F71AA1C9DF',
+	'https://zenzapis.xyz': 'BF39D349845E',
 }
 
 // Other
