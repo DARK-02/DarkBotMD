599a846e5266bfd4daf056577291c273192f95ab
Mr_Dark <76576010+DARK-02@users.noreply.github.com>
2022-04-22 09:44:03 +0700

    Create dark-phoneinfo.py

---
+++ dark-phoneinfo.py
@@ -0,0 +1,13 @@
+import phonenumbers, shlex, sys
+from phonenumbers import carrier, geocoder, timezone
+mr_dark = " ".join(map(shlex.quote, sys.argv[1:]))
+#mobileNo=input("Mobile no. with country code:")
+print (f"Info Nomor: {mr_dark}")
+mobileNo=mr_dark
+mobileNo=phonenumbers.parse(mobileNo)
+print(timezone.time_zones_for_number(mobileNo))
+print('```provider / sim```: '+carrier.name_for_number(mobileNo,"en"))
+print('```Negara```: '+geocoder.description_for_number(mobileNo,"en"))
+print("```Nomor Yang Valid?```:",phonenumbers.is_valid_number(mobileNo))
+print("```possibility```:",phonenumbers.is_possible_number(mobileNo))
+print ("_Script By MR_DARK_")
